9a97582de77533d8889cb5451f6006714b92c6ff
ruhulamin1398 <ruhul.ok@gmail.com>
2023-10-18 21:15:46 +0600

    meta musk account state change

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -14,6 +14,16 @@ function App() {
     const provider = new ethers.providers.Web3Provider(window.ethereum);
     const loadProvider= async()=>{
       if(provider){
+        window.ethereum.on('chainChanged', ()=>{
+          window.location.reload();
+        })
+
+        window.ethereum.on('accountsChanged', ()=>{
+          window.location.reload();
+        })
+
+
+
         await provider.send("eth_requestAccounts",[])
         const signer = provider.getSigner();
         const address = await signer.getAddress();
