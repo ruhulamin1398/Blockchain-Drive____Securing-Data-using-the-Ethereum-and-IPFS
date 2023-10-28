cc00bc254bb01e3878d6207206966b7cdecc3dcb
ruhulamin1398 <ruhul.ok@gmail.com>
2023-10-28 12:08:38 +0600

    change deploy port number

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,5 +3,14 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+
+
+  server: {
+    host: '0.0.0.0',  
+    port: 6666,      
+  },
+
+
+
   plugins: [react()],
 })
